96cb74e2479f76df6efc2badded18ed3bb19ec30
hakim-azizi <160917355+hakim-azizi@users.noreply.github.com>
2024-12-06 17:55:53 +0100

    add dynamic content to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,109 +1,124 @@
 import React, { useContext } from 'react';
-import { ProductContext } from '../contexts/ProductProvider';
-import Items, {ItemsProps} from '../component/Items';
-
-import '../style/home.css'
+import { ProductContext,HomeProps } from '../contexts/ProductProvider';
+import Items, { ItemsProps } from '../component/Items';
+import { ArticlesProps, FormSearch } from '../component/FormSearch';
+import '../style/home.css';
 
 const Home: React.FC = () => {
   const context = useContext(ProductContext);
-  
   if (!context) {
     return <p>Chargement des données...</p>;
   }
-  
 
   const { content, isLoading, error } = context;
 
-  if(error) return <p className='red-color'>Une Erreur c'est produite lors du chargement des données</p>
+  if (error) {
+    return (
+      <p className='red-color'>
+        Une Erreur s'est produite lors du chargement des données
+      </p>
+    );
+  }
 
-  if (isLoading || !content.items.length)  return <p>Chargement des données...</p>;
-  
-const randomiser=(max:number)=>{
-  const random:number= Math.floor(Math.random() * max);
-  return random;
-}
+  if (isLoading || !content.items.length) {
+    return <p>Chargement des données...</p>;
+  }
 
- const randomNumbers= (action:number)=> {
-    const numbers:number[] = [];
+  const randomiser = (max: number) => Math.floor(Math.random() * max);
+
+  const randomNumbers = (action: number) => {
+    const numbers: number[] = [];
     while (numbers.length < 10) {
-        const randomNumber:number = Math.floor(Math.random() * ((action-1) - 0 + 1)) + 0;
-        if (!numbers.includes(randomNumber)) {
-            numbers.push(randomNumber);
-        }
+      const randomNumber = Math.floor(Math.random() * action);
+      if (!numbers.includes(randomNumber)) {
+        numbers.push(randomNumber);
+      }
     }
     return numbers;
-}
- 
-  // Récupération des données
-  const articles: ItemsProps[] = content.items;
+  };
+
+  // Typage des données récupérées
+  const articles: ArticlesProps[] = content.items;
   const category = content.category[randomiser(content.category.length)];
   const subCategory = content.subCategory[randomiser(content.subCategory.length)];
-  
+  const homePage : HomeProps[] = content.home;
+
+  console.log('homePage => ',homePage)
+
   return (
-    <>
-      <div>
-        <header>
-          <h1 className='center'>LINEDA V&ecirc;tements</h1>
-        </header>
-        <img
-          className='visual'
-          src='asset/pictures/834e550a-baa2-490b-bbbb-50e3653674a3.jpg'
-          alt=''
-        />
-        <main>
-          <section>
-            <h2>Sous-titre</h2>
-            <p>Voici une présentation de quelques articles</p>
-            <aside className='home'>
-              {randomNumbers(content.items.length).map((rand) => (
+    <div>
+      <header>
+        <h1 className='center'>{homePage[0].sitename}</h1>
+      </header>
+      <img
+        className='visual'
+        src='asset/pictures/834e550a-baa2-490b-bbbb-50e3653674a3.jpg'
+        alt=''
+      />
+      <main>
+        <section>
+          <h2>{homePage[0].subtitle}</h2>
+          <p>{homePage[0].description}</p>
+          <p>Voici une présentation de quelques articles</p>
+          <aside className='home'>
+            {randomNumbers(content.items.length).map((rand) => {
+              // Extraction des propriétés nécessaires pour Items
+              const { name, picture, price, url }: ItemsProps = articles[rand];
+              return (
                 <Items
                   key={rand}
-                  name={articles[rand].name}
-                  picture={articles[rand].picture}
-                  price={articles[rand].price}
-                  url={articles[rand].url}
+                  name={name}
+                  picture={picture}
+                  price={price}
+                  url={url}
                 />
-              ))}
-            </aside>
-          </section>
-          <section>
-            <h2>{category.name}</h2>
-            <article>
-              <p>{category.description}</p>
-            </article>
-            <aside className='home'>
-            {randomNumbers(content.items.length).map((rand) => (
+              );
+            })}
+          </aside>
+        </section>
+        <section>
+          <h2>{category.name}</h2>
+          <article>
+            <p>{category.description}</p>
+          </article>
+          <aside className='home'>
+            {randomNumbers(content.items.length).map((rand) => {
+              const { name, picture, price, url }: ItemsProps = articles[rand];
+              return (
                 <Items
                   key={rand}
-                  name={articles[rand].name}
-                  picture={articles[rand].picture}
-                  price={articles[rand].price}
-                  url={articles[rand].url}
+                  name={name}
+                  picture={picture}
+                  price={price}
+                  url={url}
                 />
-              ))}
-            </aside>
-          </section>
-          <section>
-            <article>
-              <h2>{subCategory.name}</h2>
-              <p>{subCategory.description}</p>
-            </article>
-            <aside className='home'>
-            {randomNumbers(content.items.length).map((rand) => (
+              );
+            })}
+          </aside>
+        </section>
+        <section>
+          <article>
+            <h2>{subCategory.name}</h2>
+            <p>{subCategory.description}</p>
+          </article>
+          <aside className='home'>
+            {randomNumbers(content.items.length).map((rand) => {
+              const { name, picture, price, url }: ItemsProps = articles[rand];
+              return (
                 <Items
                   key={rand}
-                  name={articles[rand].name}
-                  picture={articles[rand].picture}
-                  price={articles[rand].price}
-                  url={articles[rand].url}
+                  name={name}
+                  picture={picture}
+                  price={price}
+                  url={url}
                 />
-              ))}
-            </aside>
-          </section>
-        </main>
-      </div>
-    </>
+              );
+            })}
+          </aside>
+        </section>
+      </main>
+    </div>
   );
-}
+};
 
 export default Home;
